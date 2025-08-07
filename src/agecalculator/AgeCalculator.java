29cfa5ca0b456cb048e14ef8d1d534d86357531f
Subham Biswal <biswalsubhamrony@gmail.com>
2025-08-07 22:48:25 +0530

    Age Calculator

---
+++ src/agecalculator/AgeCalculator.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package agecalculator;
+
+import javax.swing.SwingUtilities;
+/**
+ *
+ * @author Subham
+ */
+public class AgeCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello");
+        SwingUtilities.invokeLater(() -> {
+            new MainInterface().setVisible(true);
+        });
+    }
+    
+}
